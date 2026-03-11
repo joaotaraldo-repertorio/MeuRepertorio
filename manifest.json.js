ec549709acf33ecfa21918bdb0a4b3285fda284c
joaotaraldo-repertorio <joaotaraldo@gmail.com>
2026-03-11 15:42:20 -0300

    Add files via upload

---
+++ manifest.json.js
@@ -0,0 +1,15 @@
+{
+  "name": "Repertório João",
+  "short_name": "Repertório",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#121212",
+  "theme_color": "#cc0000",
+  "icons": [
+    {
+      "src": "https://cdn-icons-png.flaticon.com/512/3554/3554030.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'repertorio-joao-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './dados.js'
+];
+
+// O Segurança guardando as músicas no bolso na primeira vez
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// O Segurança entregando as músicas quando você estiver sem internet
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Se achou no bolso (cache), entrega. Se não, tenta buscar na internet.
+        return response || fetch(event.request);
+      })
+  );
+});
